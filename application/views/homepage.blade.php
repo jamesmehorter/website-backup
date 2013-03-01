3ac87898109c2c942333dc5f3064e8cf10696822
James <jamesmehorter@gmail.com>
2013-03-01 17:09:24 -0500

    Worked on foundation a little

--- application/views/homepage.blade.php
+++ application/views/homepage.blade.php
@@ -2,17 +2,22 @@
 
 @section('content')
 
-<form>
-	<fieldset>
-		<legend>Login</legend>
-
-		<div class="row">
-			<div class="large-12 columns">
-				<!--<label>Your Email</label>-->
-				<input type="text" placeholder="Your Email">
-			</div>
-		</div>
-	</fieldset>
-</form>
+<div class="row">
+	<div class="small-6 small-centered columns">
+		<form>
+			<fieldset>
+				<legend>Login</legend>
+				<div class="row">
+					<div class="small-6 columns">
+						<input type="email" placeholder="Your Email">
+					</div>
+					<div class="small-6 columns">
+						<input type="password" placeholder="Your Password">
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	</div>
+</div>
 
 @endsection  
